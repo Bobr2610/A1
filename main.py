f83b4e6635ec41fa929361618046d834c39a90cc
Bobr2610 <misha.sedov07@gmail.com>
2023-10-11 21:46:38 +0300

    Working chipmunks
    Done

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-def Chipmunks(a):
-    print('Чип и дейл к вам спешат')
+import wave
+import struct
+
+
+def chipmunks(a):
+    source = wave.open("in.wav", mode="rb")
+    dest = wave.open("out.wav", mode="wb")
+    dest.setparams(source.getparams())
+    frames_count = source.getnframes()
+    data = struct.unpack("<" + str(frames_count) + "h", source.readframes(frames_count))
+    newdata = data[::a]
+    newframes = struct.pack("<" + str(len(newdata)) + "h", *newdata)
+    dest.writeframes(newframes)
+    source.close()
+    dest.close()
+
+
+chipmunks(2)
